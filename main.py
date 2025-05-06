95a586cf005079cb027545d35fac183d7d505137
RzimoR <60192755+RzimoR@users.noreply.github.com>
2025-05-06 12:41:23 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# Placeholder main.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+def serve_index():
+    return FileResponse("index.html")
